e3d52cf4d546b8b557c7ad745fe3ce6072b87513
Logan Simpson <71109424+Logan-Simpson@users.noreply.github.com>
2020-10-26 09:52:10 -0600

    Add files via upload

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+var A="This is a string", C="written in Javascript";
+function myFunction() {   //function defining
+    var sent = "JavaScript demo ";    //identifying variable sent
+    sent += "learning how it works";  //adding this to the var sent
+    document.getElementById("demo").innerHTML = sent;   //this links to the html file with the "demo" ID
+}
