c38c1239c62b3021dcd18f1e7819ae7181bbe07e
Logan Simpson <71109424+Logan-Simpson@users.noreply.github.com>
2020-10-26 09:11:40 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+window.alert("Hello, World!");  //Alert window when accessing the webpage
+
+var A="This is a string", C="JavaScript"; //Defining a variable
+var A=A.fontcolor("blue");  //Changing font color for A to blue and C to green
+var C=C.fontcolor("green");
+
+document.write(A);  //Writing or printing the variables defined earlier
+document.write(C);
+document.write(3+3);
+document.write("Hello,\"World\""+"Welcome to this page");
+
+var B="Hello, world!!!!!!!!"+"What's up!!!";
+window.alert(B);
